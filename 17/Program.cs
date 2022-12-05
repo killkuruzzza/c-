182974c3ebde92307523dcd1daf9cfcefc87a424
Kirill_Moiseev_qwertyuiopoiuyhgtfdsdfgh <kilkuruzzza@mail.ru>
2022-12-05 21:31:33 +0300

    1

---
+++ 17/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if(X>0&&Y>0)
+{
+    Console.WriteLine("1");
+}
+if(X<0&&Y>0)
+{
+    Console.WriteLine("2");
+}
+if(X<0&&Y<0)
+{
+    Console.WriteLine("3");
+}
+if(X>0&&Y<0)
+{
+    Console.WriteLine("4");
+}
+if (X == 0 || Y == 0)
+    Console.WriteLine("На оси");
---
+++ 22/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+ConsoleClear();
+ConsoleWrite("Введите число N: ");
+int N = int.Parse(ConsoleReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    ConsoleWrite($"{i * i} ");
+}

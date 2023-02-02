0c3659acefff360dde94ae6f90b0e44900411ca3
Мoiseev Кirill <kilkuruzzza@mail.ru>
2023-02-02 22:00:58 +0300

    123

--- 17/Program.cs
+++ 17/Program.cs
@@ -1,25 +1,27 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+//  в которой находится эта точка.
+
+
+Console.Clear();
 Console.Write("Введите X: ");
 int X = int.Parse(Console.ReadLine());
 
 Console.Write("Введите Y: ");
 int Y = int.Parse(Console.ReadLine());
 
-if(X>0&&Y>0)
-{
+if(X>0&&Y>0){
     Console.WriteLine("1");
-}
-if(X<0&&Y>0)
-{
+}else if(X<0&&Y>0){
     Console.WriteLine("2");
-}
-if(X<0&&Y<0)
-{
+}else if(X<0&&Y<0){
     Console.WriteLine("3");
-}
-if(X>0&&Y<0)
-{
+} else if(X>0&&Y<0){
     Console.WriteLine("4");
-}
-if (X == 0 || Y == 0)
+} else if (X == 0 || Y == 0)
     Console.WriteLine("На оси");
+
+// **Задача 18:**
+
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
--- 21/Program.cs
+++ 21/Program.cs
@@ -1,13 +1,29 @@
-﻿Console.Clear();
+﻿// Напишите программу, 
+// которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 2D пространстве.
+
+// A (3,6); B(2, 1)-> 5,09
+// A(7, -5); B(1, -1)-> 7,21
+//sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
+//double s = Math.Sqrt(...);
+
+
+Console.Clear();
 Console.Write("Введите X1: ");
-int x1 = int.Parse(Console.ReadLine());
+double x1 = double.Parse(Console.ReadLine());
 Console.Write("Введите Y1: ");
-int y1 = int.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
 Console.Write("Введите X2: ");
-int x2 = int.Parse(Console.ReadLine());
+double x2 = double.Parse(Console.ReadLine());
 Console.Write("Введите Y2: ");
-int y2 = int.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
 
 double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
 
 Console.WriteLine($"d={d:f3}");
+// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
